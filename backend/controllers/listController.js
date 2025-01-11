5f30b57bb8cf89770a0c9b7bf5673ef17aecf866
Joh0 <tan_joh@hotmail.com>
2025-01-11 23:44:33 +0800

    Added listController

---
+++ backend/controllers/listController.js
@@ -0,0 +1,40 @@
+const db = require('./db'); // import the connection from db.js
+
+
+const getAllItemsByUser = (req, res) => {
+    const name = req.params.name;
+    const sortColumn = req.query.sortColumn;
+    const sortOrder = req.query.sortOrder;
+    if(!['ASC','DESC'].includes(sortOrder)){
+        return res.status(400).json({ message: "Invalid sort order"});
+    }
+    var sql = "SELECT * from to_do_items where `name` = ? and status = 'active' order by ??" + sortOrder;
+    db.query(sql, [name, sortColumn], (err, result) => {
+        if(err){
+            res.status(500).json({ message: 'Error reading from DB', error: err.message});
+        }
+        else{
+            res.status(200).json({ data: result });
+        }
+    });
+}
+
+const getAllItemsByUserGroup = (req, res) => {
+    const group = req.params.group;
+    const sortColumn = req.query.sortColumn;
+    const sortOrder = req.query.sortOrder;
+    if(!['ASC','DESC'].includes(sortOrder)){
+        return res.status(400).json({ message: "Invalid sort order"});
+    }
+    var sql = "SELECT * from to_do_items where `group` = ? and status = 'active' order by ??" + sortOrder;
+    db.query(sql, [group, sortColumn], (err, result) => {
+        if(err){
+            res.status(500).json({ message: 'Error reading from DB', error: err.message});
+        }
+        else{
+            res.status(200).json({ data: result });
+        }
+    });
+}
+
+module.exports = { getAllItemsByUser, getAllItemsByUserGroup };
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express'); // All of these you have to 'npm install xxx' at the base path
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { getAllItemsByUser } = require('./controllers/basicController');
+const { getAllItemsByUser, getAllItemsByUserGroup } = require('./controllers/listController');
 
 const app = express();
 
@@ -10,7 +10,8 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // All the apis
-app.get("/api/list/:name", getAllItemsByUser);
+app.get("/api/listName/:name", getAllItemsByUser);
+app.get("/api/listGroup/:group", getAllItemsByUserGroup);
 
 const PORT = 3000;
 app.listen(PORT, ()=>{
